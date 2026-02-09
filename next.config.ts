a518848be22c512c19f229292131f06514a9618a
vinceOJT <jaca.ojt@gmail.com>
2026-02-09 23:00:47 +0800

    direct connection instead of referencing from env

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: process.env.NEXT_PUBLIC_N8N_PATH!,
-        destination: process.env.NEXT_PUBLIC_N8N_API!,
+        source: "/api/v1",
+        destination: "https://n8n.callboxinc.com/webhook/api/v1",
       }
     ]
   }

67fa776340e4484cad23f4540eb9f2f1df6caec8
vinceOJT <jaca.ojt@gmail.com>
2026-02-10 23:33:39 +0800

    change env reference to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/v1",
-        destination: "https://n8n.callboxinc.com/webhook/api/v1",
+        source: "/lead-gen/blogs", // can be anything is public anyway
+        destination: process.env.NEXT_PUBLIC_N8N_API!,
       }
     ]
   }
